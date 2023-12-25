9fafc107f5ea17109c2dab8f2ec5a40d948c1b6b
ali <ali.m.radwan123@gmail.com>
2023-12-25 15:42:28 +0300

    project main-page

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-
-createApp(App).use(router).mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./assets/tailwind.css";
+import { createPinia } from "pinia";
+const pinia = createPinia();
+const app = createApp(App);
+app.use(pinia);
+createApp(App).use(router).use(pinia).mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,41 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import MainLayout from "../layout/main.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/login",
+    name: "login",
+    component: () => import("../views/loginView.vue"),
   },
+
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    path: "/",
+    component: MainLayout,
+    redirect: "/home",
+    children: [
+      {
+        path: "/home",
+        name: "home",
+        component: HomeView,
+      },
+      {
+        path: "/list",
+        name: "list",
+        component: () => import("../views/listView.vue"),
+      },
+      {
+        path: "/deatails",
+        name: "deatails",
+        component: () => import("../views/deatailsView.vue"),
+      },
+    ],
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
